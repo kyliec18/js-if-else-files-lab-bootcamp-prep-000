6d4e0a24ddf83367c9cb20a5696a3c00f6a5b5dd
kyliec18 <kyliecox2012@gmail.com>
2017-09-21 21:59:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var name = "Susan"
-var height = "74"
+var height = "70"
 if (name === "Susan"){
   console.log("Name: " + name)
+} elseif (height === "70"){
+  console.log("Height: " + height)
 } else {
   console.log("Expected the name to be Susan")
 }
